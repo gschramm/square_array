28a65cde0c64c1b962ea8f84e8c77f95ddf5cd80
Georg Schramm <georg@Georgs-MacBook-Pro-208.local>
2025-04-09 18:51:42 +0100

    mv ray cube intersection to utils

--- joseph3d_fwd_worker.h
+++ joseph3d_fwd_worker.h
@@ -1,74 +1,12 @@
 #pragma once
 #include "cuda_compat.h"
 #include "atomic_sum.h"
+#include "utils.h"
 
 // we need to import math.h for the floor, ceil, sqrtf if not compiling for CUDA
 #ifndef __CUDA_ARCH__
 #include <math.h>
 #endif
-
-WORKER_QUALIFIER unsigned char ray_cube_intersection(float orig0,
-                                                     float orig1,
-                                                     float orig2,
-                                                     float bounds0_min,
-                                                     float bounds1_min,
-                                                     float bounds2_min,
-                                                     float bounds0_max,
-                                                     float bounds1_max,
-                                                     float bounds2_max,
-                                                     float rdir0,
-                                                     float rdir1,
-                                                     float rdir2,
-                                                     float* t1,
-                                                     float* t2){
-  // the inverse of the directional vector
-  // using the inverse of the directional vector and IEEE floating point arith standard 754
-  // makes sure that 0's in the directional vector are handled correctly 
-  float invdir0 = 1.f/rdir0;
-  float invdir1 = 1.f/rdir1;
-  float invdir2 = 1.f/rdir2;
-  
-  unsigned char intersec = 1;
-  
-  float t11, t12, t21, t22; 
-
-  if (invdir0 >= 0){
-    *t1  = (bounds0_min - orig0) * invdir0;
-    *t2  = (bounds0_max - orig0) * invdir0; 
-  }
-  else{
-    *t1  = (bounds0_max - orig0) * invdir0;
-    *t2  = (bounds0_min - orig0) * invdir0;
-  }
-  
-  if (invdir1 >= 0){
-    t11 = (bounds1_min - orig1) * invdir1; 
-    t12 = (bounds1_max - orig1) * invdir1; 
-  }
-  else{
-    t11 = (bounds1_max - orig1) * invdir1;
-    t12 = (bounds1_min - orig1) * invdir1; 
-  }
-  
-  if ((*t1 > t12) || (t11 > *t2)){intersec = 0;}
-  if (t11 > *t1){*t1 = t11;}
-  if (t12 < *t2){*t2 = t12;}
-  
-  if (invdir2 >= 0){
-    t21 = (bounds2_min - orig2) * invdir2; 
-    t22 = (bounds2_max - orig2) * invdir2;
-  } 
-  else{
-    t21 = (bounds2_max - orig2) * invdir2; 
-    t22 = (bounds2_min - orig2) * invdir2;
-  } 
-  
-  if ((*t1 > t22) || (t21 > *t2)){intersec = 0;}
-  if (t21 > *t1){*t1 = t21;}
-  if (t22 < *t2){*t2 = t22;} 
-
-  return(intersec);
-}
 
 WORKER_QUALIFIER inline void joseph3d_fwd_worker(size_t i,
                                                  const float *xstart, 
