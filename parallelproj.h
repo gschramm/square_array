6fcd5f658c0b1355fc28568dea0890f9573a8823
Georg Schramm <georg.schramm@kuleuven.be>
2025-04-09 13:19:21 +0200

    add example on how to use fwd projector with cuda malloc managed pointers

---
+++ parallelproj.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <cstddef>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void joseph3d_fwd_cuda(const float *xstart, 
+                       const float *xend, 
+                       const float *img,
+                       const float *img_origin, 
+                       const float *voxsize, 
+                       float *p,
+                       long long nlors, 
+                       const int *img_dim,
+                       int device_id = 0,
+                       int threadsperblock = 64);
+
+#ifdef __cplusplus
+}
+#endif
