aa8a8859dfc1e0c359d0330eed228520301477b8
Georg Schramm <georg.schramm@kuleuven.be>
2025-04-14 11:31:33 +0200

    add more cases for handle_cuda_input_array

--- include/cuda_utils.h
+++ include/cuda_utils.h
@@ -2,5 +2,10 @@
 
 #include <cuda_runtime.h>
 
+// Overload for constant input_ptr (const T*)
 template <typename T>
-void handle_cuda_input_array(const T *host_ptr, T **device_ptr, size_t size, bool &free_flag, int device_id, cudaMemoryAdvise memory_hint);
+void handle_cuda_input_array(const T *input_ptr, T **device_ptr, size_t size, bool &free_flag, int device_id, cudaMemoryAdvise memory_hint);
+
+// Overload for non-constant input_ptr (T*)
+template <typename T>
+void handle_cuda_input_array(T *input_ptr, T **device_ptr, size_t size, bool &free_flag, int device_id, cudaMemoryAdvise memory_hint);
--- src/cuda_utils.cu
+++ src/cuda_utils.cu
@@ -1,31 +1,81 @@
 #include "cuda_utils.h"
+#include <iostream>
 
+// Overload for constant input_ptr (const T*)
 template <typename T>
-void handle_cuda_input_array(const T *host_ptr, T **device_ptr, size_t size, bool &free_flag, int device_id, cudaMemoryAdvise memory_hint)
+void handle_cuda_input_array(const T *input_ptr, T **device_ptr, size_t size, bool &free_flag, int device_id, cudaMemoryAdvise memory_hint)
 {
     cudaPointerAttributes attr;
-    cudaError_t err = cudaPointerGetAttributes(&attr, host_ptr);
+    cudaError_t err = cudaPointerGetAttributes(&attr, input_ptr);
     free_flag = false;
 
     if (err == cudaSuccess && attr.type == cudaMemoryTypeManaged)
     {
-        cudaMemPrefetchAsync(const_cast<void *>(static_cast<const void *>(host_ptr)), size, device_id);
-        cudaMemAdvise(const_cast<void *>(static_cast<const void *>(host_ptr)), size, memory_hint, device_id);
+        // Prefetch and advise for managed memory
+        cudaMemPrefetchAsync(const_cast<void *>(static_cast<const void *>(input_ptr)), size, device_id);
+        cudaMemAdvise(const_cast<void *>(static_cast<const void *>(input_ptr)), size, memory_hint, device_id);
     }
 
     if (err == cudaSuccess && (attr.type == cudaMemoryTypeManaged || attr.type == cudaMemoryTypeDevice))
     {
-        *device_ptr = const_cast<T *>(host_ptr);
+        // Assign managed or device pointer
+        *device_ptr = const_cast<T *>(input_ptr);
     }
     else
     {
         // Host pointer case, transfer to device
         cudaMalloc(device_ptr, size);
-        cudaMemcpy(*device_ptr, host_ptr, size, cudaMemcpyHostToDevice);
+        cudaMemcpy(*device_ptr, input_ptr, size, cudaMemcpyHostToDevice);
+        free_flag = true;
+    }
+}
+
+// Overload for non-constant input_ptr (T*) (not const)
+template <typename T>
+void handle_cuda_input_array(T *input_ptr, T **device_ptr, size_t size, bool &free_flag, int device_id, cudaMemoryAdvise memory_hint)
+{
+    cudaPointerAttributes attr;
+    cudaError_t err = cudaPointerGetAttributes(&attr, input_ptr);
+    free_flag = false;
+
+    if (err == cudaSuccess && attr.type == cudaMemoryTypeManaged)
+    {
+        // Prefetch and advise for managed memory
+        cudaMemPrefetchAsync(input_ptr, size, device_id);
+        cudaMemAdvise(input_ptr, size, memory_hint, device_id);
+    }
+
+    if (err == cudaSuccess && (attr.type == cudaMemoryTypeManaged || attr.type == cudaMemoryTypeDevice))
+    {
+        // Assign managed or device pointer
+        *device_ptr = input_ptr;
+    }
+    else
+    {
+        // Host pointer case, transfer to device
+        cudaMalloc(device_ptr, size);
+        cudaMemcpy(*device_ptr, input_ptr, size, cudaMemcpyHostToDevice);
         free_flag = true;
     }
 }
 
 // Explicit template instantiations
+template void handle_cuda_input_array<double>(const double *, double **, size_t, bool &, int, cudaMemoryAdvise);
+template void handle_cuda_input_array<double>(double *, double **, size_t, bool &, int, cudaMemoryAdvise);
 template void handle_cuda_input_array<float>(const float *, float **, size_t, bool &, int, cudaMemoryAdvise);
+template void handle_cuda_input_array<float>(float *, float **, size_t, bool &, int, cudaMemoryAdvise);
+
 template void handle_cuda_input_array<int>(const int *, int **, size_t, bool &, int, cudaMemoryAdvise);
+template void handle_cuda_input_array<int>(int *, int **, size_t, bool &, int, cudaMemoryAdvise);
+template void handle_cuda_input_array<unsigned int>(const unsigned int *, unsigned int **, size_t, bool &, int, cudaMemoryAdvise);
+template void handle_cuda_input_array<unsigned int>(unsigned int *, unsigned int **, size_t, bool &, int, cudaMemoryAdvise);
+template void handle_cuda_input_array<size_t>(const size_t *, size_t **, size_t, bool &, int, cudaMemoryAdvise);
+template void handle_cuda_input_array<size_t>(size_t *, size_t **, size_t, bool &, int, cudaMemoryAdvise);
+
+template void handle_cuda_input_array<char>(const char *, char **, size_t, bool &, int, cudaMemoryAdvise);
+template void handle_cuda_input_array<char>(char *, char **, size_t, bool &, int, cudaMemoryAdvise);
+template void handle_cuda_input_array<unsigned char>(const unsigned char *, unsigned char **, size_t, bool &, int, cudaMemoryAdvise);
+template void handle_cuda_input_array<unsigned char>(unsigned char *, unsigned char **, size_t, bool &, int, cudaMemoryAdvise);
+
+template void handle_cuda_input_array<bool>(const bool *, bool **, size_t, bool &, int, cudaMemoryAdvise);
+template void handle_cuda_input_array<bool>(bool *, bool **, size_t, bool &, int, cudaMemoryAdvise);
